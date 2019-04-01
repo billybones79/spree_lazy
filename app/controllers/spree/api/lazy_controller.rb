5f8395de4eaa7165c195ab2004ca6d244896ba3a
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-01 16:17:58 -0400

    made better

--- app/controllers/spree/api/lazy_controller.rb
+++ app/controllers/spree/api/lazy_controller.rb
@@ -7,7 +7,7 @@ module Spree
       def product
 
         I18n.locale = session[:locale] = params[:locale]
-        @product = Spree::Product.includes(:variant_images, :taxons, :master_variant, {in_stock_variants: { active_sale_prices: {}, stock_items: {}, option_values:{translations: {}, option_type: :translations} }}).find(params[:id])
+        @product = Spree::Product.available.includes(:variant_images, :taxons, :master_variant, {in_stock_variants: { active_sale_prices: {}, stock_items: {}, option_values:{translations: {}, option_type: :translations} }}).find(params[:id])
 
 
         render partial: "spree/shared/single_product", locals:{:product => @product}, :layout => false
