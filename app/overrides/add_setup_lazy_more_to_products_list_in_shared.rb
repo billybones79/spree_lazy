ec9b2d985443b0fc046f338ecc00ebe1415673cd
Quantumphilisp <quantumphilisp@gmail.com>
2019-04-24 16:12:46 -0400

    fixed some html and css

--- app/overrides/add_setup_lazy_more_to_products_list_in_shared.rb
+++ app/overrides/add_setup_lazy_more_to_products_list_in_shared.rb
@@ -6,8 +6,8 @@
 
   Deface::Override.new(:virtual_path=>'spree/shared/_products_list_infinite',
                        :name => 'replace_content_product_list',
-                       :replace =>"div.col-md-3",
-                       :text         => '<div class = "col-md-3 product-list-item">
+                       :replace =>".product-list-item",
+                       :text         => '<div class = "col-md-4 product-list-item">
               <div class="col lazy-partial"  data-lazy-url="<%= lazy_product_path(product.id)%>" >
                 <div id="product_<%= product.id %>" class="single-product =<%=cycle("alpha", "secondary", "", "omega secondary", :name => "classes") %>"
   data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
